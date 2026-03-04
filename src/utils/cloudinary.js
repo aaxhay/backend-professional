20e4c01f25f253ed4379736b458b8f8d79f75396
aaxhay <aashaydongre11@gmail.com>
2026-03-04 14:27:53 +0530

    refactor: replace hardcoded Cloudinary credentials with environment variables

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: "dojdsblan",//process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: "441564532749167",//process.env.CLOUDINARY_API_KEY,
-  api_secret: "gylctVKrUeccs4aMR9q6JwFcZfQ"//process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
